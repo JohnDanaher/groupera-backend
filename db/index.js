c2f2e274fcafc64d242e6f18aeba0f6986a2bf22
John <jayblez@gmail.com>
2023-12-08 17:47:46 +0100

    um

--- db/index.js
+++ db/index.js
@@ -7,12 +7,17 @@ const mongoose = require('mongoose')
 
 const MONGO_URI = process.env.DB_CLUSTER
 
-mongoose
-	.connect(MONGO_URI)
-	.then((x) => {
-		const dbName = x.connections[0].name
-		console.log(`Connected to Mongo! Database name: "${dbName}"`)
-	})
-	.catch((err) => {
-		console.error('Error connecting to mongo: ', err)
-	})
+const connectDatabase = async () => {
+	try {
+		mongoose.set('useNewUrlParser', true)
+
+		await mongoose.connect(MONGO_URI)
+
+		console.log('connected to database')
+	} catch (error) {
+		console.log(error)
+		process.exit(1)
+	}
+}
+
+connectDatabase()
